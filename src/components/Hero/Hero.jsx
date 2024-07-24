bef7265eb603709f63e48e259bf1e52be6ada046
aashisarbhai <aashi.sarbhai@gmail.com>
2024-07-24 20:25:51 +0530

    Improved styling + added footer

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,34 +1,21 @@
 import React from 'react'
 import './Hero.css'
  import photo1 from '../photo/photo1.jpg'
- import photo2 from '../photo/photo2.jpg'
- import photo3 from '../photo/photo3.jpg'
 
 const Hero = () => {
   return (
     <div className='hero'>
       <div className="hero-left">
         <h2>New Arrivals</h2>
-      <div>
+        <div>
           <div className="hand-hand-icon">
               <p>new</p>
               <img src={photo1} alt="" ></img>
           </div>
-          <p>collection</p>
-          <p>for everyone</p>
-      </div>
-      <div className="hero-atest-btn">
-         <div>
-         Latest Collection
-         </div>
-         <img src={photo2} alt="vugv" />
-      </div>
-      </div>
+</div>
+</div>
 
-      <div className="hero-right">
-      <img src={photo3} alt="" />
-      </div>
-      </div>     
+    </div>    
   )
 }
 export default Hero;

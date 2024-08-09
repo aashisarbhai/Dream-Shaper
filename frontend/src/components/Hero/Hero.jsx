dd92787d05d229eff000ce579e624842b8077bc9
aashisarbhai <aashi.sarbhai@gmail.com>
2024-08-09 13:07:17 +0530

    more updates

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
-import './Hero.css'
- import hero2 from '../../components/photo/hero2.png'
+import React from 'react';
+import Slider from 'react-slick';
+import hero1 from '../photo/hero1.png';
+import hero2 from '../photo/hero2.png';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './Hero.css';
 
-const Hero = () => {
+function Hero() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+  };
+
   return (
-    <div className='hero'>
+    <div className="hero-container">
+      <Slider {...settings}>
         <div>
-          <div className="hand-hand-icon">
-              <img src={hero2} alt="" ></img>
-          </div>
-</div>
-</div>   
-  )
+          <img src={hero1} alt="Slide 1" />
+        </div>
+        <div>
+          <img src={hero2} alt="Slide 2" />
+        </div>
+      </Slider>
+    </div>
+  );
 }
+
 export default Hero;

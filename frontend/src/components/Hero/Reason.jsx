610e6e20ee9b23ecf2d32631ef173b43a6b1b294
aashisarbhai <aashi.sarbhai@gmail.com>
2024-08-14 12:49:25 +0530

    done

--- frontend/src/components/Hero/Reason.jsx
+++ frontend/src/components/Hero/Reason.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import reason from '../photo/reason.png'
+import reason1 from '../photo/reason1.png'
 import './Reason.css';
 
 function Reason() {
   return (
     <div>
       <div className="container">
-        <img src={reason} alt=" " />
+        <img src={reason1} alt=" " />
       </div>
     </div>
   )
